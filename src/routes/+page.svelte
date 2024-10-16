b2589c6b88ea9052198ec7459c4c636a1ac4dba0
Felix Olesen <felixolesen@gmail.com>
2024-10-16 13:46:43 +0800

    Added some dynamic transitions between hover states on the project cards

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -36,46 +36,11 @@
                 <ToggleGroup.Item value="projects" class="rounded-xl bg-secondary shadow-xl">Projects</ToggleGroup.Item>
             </ToggleGroup.Root>
         </div>
-        <div class="flex flex-row w-screen h-4/5 overflow-x-scroll hideScroll space-x-6 mb-8 px-6">
+        <div class="flex flex-row w-screen h-4/5 overflow-x-scroll hideScroll space-x-1 mb-8 px-6">
             <Projects on:cardHover={handleCardHover}></Projects>
         </div>
     </div>
 </div>
-
-
-<!-- <div class="flex flex-row">
-    <div class="flex flex-col w-1/2 pt-12 items-center">
-        <div class="flex flex-row items-center space-x-4 pt-10 pb-4  w-3/5">
-            <div class="">
-                <Avatar.Root class="h-24 w-24">
-                    <Avatar.Image src={image} alt="FO" />
-                    <Avatar.Fallback>FO</Avatar.Fallback>
-                </Avatar.Root>
-            </div>
-            <div class="flex flex-col">
-                <h1 class="scroll-m-20 text-2xl tracking-tight lg:text-4xl">
-                    Felix Sylvest Olesen
-                </h1>
-                <h2 class="scroll-m-20 text-l tracking-tight">
-                    {info.title}
-                </h2>
-            </div>
-        </div>
-        <div class="w-3/5">
-            {#if activeProject != null}
-                <ProjectOverview project={activeProject}></ProjectOverview>
-            {/if}
-        </div>
-        <div class="flex flex-col h-full justify-end w-3/5">
-            <Socials></Socials>
-        </div>
-    </div>
-    <div class="flex min-h-screen w-1/2 flex-col pt-0 pr-12">
-        <div class="overflow-scroll hideScroll w-3/4 h-screen p-2">
-            <Projects on:cardHover={handleCardHover}></Projects>
-        </div>
-    </div>
-</div> -->
 
 <style>
 	.hideScroll::-webkit-scrollbar { display: none; }
--- src/routes/resumeCards.svelte
+++ src/routes/resumeCards.svelte
@@ -5,37 +5,81 @@
     import CardContent from "$lib/components/ui/card/card-content.svelte";
 
     const dispatch = createEventDispatcher();
-
+    
+    let hovering: boolean = false;
+    let currentProject: any = null;
+    
     export let cardList;
-    function handleHover(project:any) {
-        console.log("I'm working!");
+
+
+    function handleHoverEnter(project:any) {
+        console.log("Entering!");
+        hovering = true;
+        currentProject = project;
         dispatch('cardHover', {
             project: project
         });
     }
-  
-</script>
+    
+    function handleHoverLeave() {
+        console.log("Leaving!");
+        hovering = false;
+        currentProject = null;
 
+    }
+
+</script>
 {#each cardList as project}
 <!-- svelte-ignore a11y-no-static-element-interactions -->
-<!-- <a on:click={() => handleClick(project.role)} on:keypress={() => handleClick(project.role)} href="/"> -->
-<Card.Root class="min-w-96 max-h-48 rounded-xl hover:bg-primary border-0 shadow-inner">
-    <div class="scroller" on:mouseenter={() => handleHover(project)}>
-        <Card.Header class="">
-            <Card.Title><h1>{project.role}</h1></Card.Title>
-            <Card.Description><p class="mx-3">{project.description}</p></Card.Description>
-        </Card.Header>
-        <Card.Content class=" p-1">
-            <!-- <p>{project.description}</p> -->
-        </Card.Content>
-        <Card.Footer class="flex flex-row flex-wrap space-x-1 space-y-1 items-center ">
-            {#each project.tags as tag}
-                <Badge variant="outline" class="justify-center mt-1 bg-primary">{tag}</Badge>
-            {/each}
-        </Card.Footer>
+<div class="transition ease-in-out min-w-96 scale-90 max-h-52 hover:scale-100" on:mouseenter={() => handleHoverEnter(project)} on:mouseleave={handleHoverLeave}>
+    <div class="flex flex-col h-full w-full p-3 space-y-2 rounded-xl shadow-inner bg-primary scroller justify-center items-center transition-all ease-in-out hover:border hover:shadow-md hover:shadow-primary hover:justify-start ">
+        
+        <!-- Header -->
+        {#if hovering && currentProject === project}
+            <div class="flex flex-row  w-full">
+                <div class=" w-1/2 flex flex-row items-center">
+                    <!-- Title -->
+                    <div class=""><h1 class="text-md font-bold">{project.role}</h1></div>
+                </div>
+                <div class="w-1/2 flex flex-row place-content-end items-center space-x-4">
+                    <!-- Duration -->
+                    <div class=""><h3 class="text-sm font-light"> {project.duration}</h3></div>
+                    <!-- Expand Icon -->
+                    <div class=""><svg fill="#509532" height="18" width="18" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 242.133 242.133" xml:space="preserve"><path id="XMLID_15_" d="M227.133,83.033c8.283,0,15-6.716,15-15V15c0-8.284-6.717-15-15-15H174.1c-8.284,0-15,6.716-15,15s6.716,15,15,15h16.82l-69.854,69.854L51.213,30h16.82c8.284,0,15-6.716,15-15s-6.716-15-15-15H15C6.717,0,0,6.716,0,15v53.033c0,8.284,6.717,15,15,15c8.285,0,15-6.716,15-15v-16.82l69.854,69.854L30,190.92V174.1c0-8.284-6.715-15-15-15c-8.283,0-15,6.716-15,15v53.033c0,8.284,6.717,15,15,15h53.033c8.284,0,15-6.716,15-15c0-8.284-6.716-15-15-15h-16.82l69.854-69.854l69.854,69.854H174.1c-8.284,0-15,6.716-15,15c0,8.284,6.716,15,15,15h53.033c8.283,0,15-6.716,15-15V174.1c0-8.284-6.717-15-15-15c-8.285,0-15,6.716-15,15v16.82l-69.854-69.854l69.854-69.854v16.82C212.133,76.317,218.848,83.033,227.133,83.033z"/></svg></div>    
+                </div>           
+            </div>
+        {:else if currentProject !== project}
+            <!-- Title -->
+            <div class="flex flex-col w-full items-center">
+                    <div class=""><h1 class="text-xl font-bold">{project.role}</h1></div>
+            </div>
+        {/if}
+
+        <!-- Content -->
+        {#if hovering && currentProject === project}
+            <div class="w-full h-full">
+                <div class="mx-3"><p class="text-sm">{project.description}</p></div>
+            </div>
+        {/if}
+
+        <!-- Footer -->
+        {#if hovering && currentProject === project}
+            <div class="flex flex-row flex-wrap w-full items-end space-x-1 space-y-1 pb-2">
+                {#each project.tags as tag}
+                    <Badge variant="outline" class="justify-center bg-primary h-fit">{tag}</Badge>
+                {/each}
+            </div>
+        {:else if currentProject !== project}
+            <div class="flex flex-row flex-wrap w-full items-end justify-center space-x-1 space-y-1 pb-2">
+                {#each project.tags as tag}
+                    <Badge variant="outline" class="justify-center bg-primary h-fit">{tag}</Badge>
+                {/each}
+            </div>
+        {/if}
+
     </div>
-</Card.Root>
-<!-- </a> -->
+</div>
+
 {/each}
 
 <style>
