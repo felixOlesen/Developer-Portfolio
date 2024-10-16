b6ffb40497c7ae67289a4a879d9a7259354d9eb1
Felix Olesen <felixolesen@gmail.com>
2024-10-16 16:45:00 +0800

    Implemented some hover transitions for the About me button and social media buttons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,6 +7,10 @@
 
     $: activeProject = null;
 
+    function displayAboutMe() {
+
+    }
+
     function handleCardHover(event:any) {
         activeProject = event.detail.project;
     }
@@ -22,10 +26,12 @@
                 <h1 class="text-3xl"> Hi, I'm</h1>
                 <h1 class="text-5xl font-semibold"> Felix Sylvest Olesen</h1>
                 <h1 class="text-2xl"> Computer Science Graduate</h1>
-                <div class="flex flex-row w-fit bg-primary rounded-xl shadow-inner p-2 space-x-2 items-center">
-                    <svg fill="#509532" height="18" width="18" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 242.133 242.133" xml:space="preserve"><path id="XMLID_15_" d="M227.133,83.033c8.283,0,15-6.716,15-15V15c0-8.284-6.717-15-15-15H174.1c-8.284,0-15,6.716-15,15s6.716,15,15,15h16.82l-69.854,69.854L51.213,30h16.82c8.284,0,15-6.716,15-15s-6.716-15-15-15H15C6.717,0,0,6.716,0,15v53.033c0,8.284,6.717,15,15,15c8.285,0,15-6.716,15-15v-16.82l69.854,69.854L30,190.92V174.1c0-8.284-6.715-15-15-15c-8.283,0-15,6.716-15,15v53.033c0,8.284,6.717,15,15,15h53.033c8.284,0,15-6.716,15-15c0-8.284-6.716-15-15-15h-16.82l69.854-69.854l69.854,69.854H174.1c-8.284,0-15,6.716-15,15c0,8.284,6.716,15,15,15h53.033c8.283,0,15-6.716,15-15V174.1c0-8.284-6.717-15-15-15c-8.285,0-15,6.716-15,15v16.82l-69.854-69.854l69.854-69.854v16.82C212.133,76.317,218.848,83.033,227.133,83.033z"/></svg>
-                    <h1 class="text-l text-primary-foreground"> More about me</h1>
-                </div>
+                <a href="/">
+                    <div class="flex flex-row w-fit bg-primary rounded-xl shadow-inner p-2 space-x-2 items-center group transition ease-in-out hover:scale-105 ">
+                        <svg class="transition ease-in-out group-hover:scale-105" fill="#509532" height="18" width="18" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 242.133 242.133" xml:space="preserve"><path id="XMLID_15_" d="M227.133,83.033c8.283,0,15-6.716,15-15V15c0-8.284-6.717-15-15-15H174.1c-8.284,0-15,6.716-15,15s6.716,15,15,15h16.82l-69.854,69.854L51.213,30h16.82c8.284,0,15-6.716,15-15s-6.716-15-15-15H15C6.717,0,0,6.716,0,15v53.033c0,8.284,6.717,15,15,15c8.285,0,15-6.716,15-15v-16.82l69.854,69.854L30,190.92V174.1c0-8.284-6.715-15-15-15c-8.283,0-15,6.716-15,15v53.033c0,8.284,6.717,15,15,15h53.033c8.284,0,15-6.716,15-15c0-8.284-6.716-15-15-15h-16.82l69.854-69.854l69.854,69.854H174.1c-8.284,0-15,6.716-15,15c0,8.284,6.716,15,15,15h53.033c8.283,0,15-6.716,15-15V174.1c0-8.284-6.717-15-15-15c-8.285,0-15,6.716-15,15v16.82l-69.854-69.854l69.854-69.854v16.82C212.133,76.317,218.848,83.033,227.133,83.033z"/></svg>
+                        <h1 class="transition ease-in-out text-l text-primary-foreground group-hover:scale-95"> More about me</h1>
+                    </div>
+                </a>
             </div>
         </div>
     </div>
