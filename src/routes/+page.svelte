a2b5a00c019a6ddd45c14da65d492523614284e3
Felix Olesen <felixolesen@gmail.com>
2024-10-21 13:11:55 +0800

    Updated Horizontal Scrollbar Styling to be more stealthy

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -19,7 +19,7 @@
                 <a href="/">
                     <div class="flex flex-row w-fit bg-primary rounded-xl shadow-inner p-2 space-x-2 items-center group transition ease-in-out hover:scale-105 ">
                         <svg class="transition ease-in-out group-hover:scale-105" fill="#509532" height="18" width="18" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 242.133 242.133" xml:space="preserve"><path id="XMLID_15_" d="M227.133,83.033c8.283,0,15-6.716,15-15V15c0-8.284-6.717-15-15-15H174.1c-8.284,0-15,6.716-15,15s6.716,15,15,15h16.82l-69.854,69.854L51.213,30h16.82c8.284,0,15-6.716,15-15s-6.716-15-15-15H15C6.717,0,0,6.716,0,15v53.033c0,8.284,6.717,15,15,15c8.285,0,15-6.716,15-15v-16.82l69.854,69.854L30,190.92V174.1c0-8.284-6.715-15-15-15c-8.283,0-15,6.716-15,15v53.033c0,8.284,6.717,15,15,15h53.033c8.284,0,15-6.716,15-15c0-8.284-6.716-15-15-15h-16.82l69.854-69.854l69.854,69.854H174.1c-8.284,0-15,6.716-15,15c0,8.284,6.716,15,15,15h53.033c8.283,0,15-6.716,15-15V174.1c0-8.284-6.717-15-15-15c-8.285,0-15,6.716-15,15v16.82l-69.854-69.854l69.854-69.854v16.82C212.133,76.317,218.848,83.033,227.133,83.033z"/></svg>
-                        <h1 class="transition ease-in-out text-l text-primary-foreground group-hover:scale-95"> More about me</h1>
+                        <h1 class="transition ease-in-out text-l text-border group-hover:text-primary-foreground group-hover:scale-95"> More about me</h1>
                     </div>
                 </a>
             </div>
@@ -32,7 +32,7 @@
                 <ToggleGroup.Item value="projects" class="rounded-xl bg-secondary shadow-xl">Projects</ToggleGroup.Item>
             </ToggleGroup.Root>
         </div>
-        <div class="flex flex-row w-screen h-4/5 overflow-x-scroll scroller space-x-1 mb-8 px-6">
+        <div class="flex flex-row w-screen h-4/5 overflow-x-scroll space-x-1 mb-8 px-6">
             <Projects resumeData={data}></Projects>
         </div>
     </div>
@@ -41,9 +41,48 @@
 
 
 <style>
-    div.scroller {
+    /* div.scroller {
+        
         scrollbar-color: rgba(80,149,50,1) rgba(49,49,49,1);
         scroll-behavior: smooth;
         scrollbar-gutter: stable;
-    } 
+    }  */
+
+    /* width */
+::-webkit-scrollbar {
+    width: 20px;
+    height: 10px;
+}
+
+/* Track */
+::-webkit-scrollbar-track {
+    background: rgba(66,65,65,1);
+    border-radius: 10px;
+    margin-left: 45px;
+    margin-right: 45px;
+}
+
+::-webkit-scrollbar-track:hover {
+    box-shadow: inset 0 0 5px rgba(49,49,49,1);
+    box-shadow: inset 0 0 5px rgba(49,49,49,1);
+    background: rgba(49,49,49,0.5);
+}
+
+/* Handle */
+::-webkit-scrollbar-thumb {
+    background: rgba(49,49,49,0.5);
+    border-radius: 10px;
+    width: auto;
+    height: 50%;
+
+}
+
+::-webkit-scrollbar-thumb:hover {
+    background: rgba(80,149,50,0.7);
+    border-radius: 10px;
+    width: auto;
+    height: 50%;
+
+}
+
 </style>
